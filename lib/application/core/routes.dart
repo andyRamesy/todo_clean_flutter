460e032dba73554b063a0dabf580572e358c0cd0
mahery <mahery@futurmap.com>
2024-11-11 17:29:33 +0300

    add observer

--- lib/application/core/routes.dart
+++ lib/application/core/routes.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:todo_clean/application/core/go_router_observer.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'root');
 
 final routes = GoRouter(
-  navigatorKey: _rootNavigatorKey,
-  initialLocation: '/home',
+  initialLocation: '/home/start',
+  observers: <NavigatorObserver>[GoRouterObserver()],
   routes: [
     GoRoute(
       path: '/home/settings',
       builder: (context, state) {
         return Container(
           color: Colors.amber,
+          child: Column(
+            key: _rootNavigatorKey,
+            children: [
+              ElevatedButton(
+                  onPressed: () => context.go('/home/start'),
+                  child: const Text('Go to start')),
+              TextButton(
+                  onPressed: () => context.pop(), child: const Text('Go back'))
+            ],
+          ),
         );
       },
     ),
@@ -21,6 +32,16 @@ final routes = GoRouter(
       builder: (context, state) {
         return Container(
           color: Colors.blueGrey,
+          child: Column(
+            key: _rootNavigatorKey,
+            children: [
+              ElevatedButton(
+                  onPressed: () => context.go('/home/settings'),
+                  child: const Text('Go to settings')),
+              TextButton(
+                  onPressed: () => context.pop(), child: const Text('Go back'))
+            ],
+          ),
         );
       },
     )
