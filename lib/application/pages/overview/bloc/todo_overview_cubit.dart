d034c682d8e15b319756542c6a4e043353e161b2
andy ramesy <andyramesy@outlook.fr>
2024-11-14 22:46:30 +0300

    stop on todo overview cubit

---
+++ lib/application/pages/overview/bloc/todo_overview_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_clean/application/pages/overview/bloc/todo_overview_cubit_state.dart';
+
+part 'todo_overview_cubit_state.dart';
+
+class TodoOverviewCubit extends Cubit<TodoOverviewCubitState> {
+  
+}
---
+++ lib/application/pages/overview/bloc/todo_overview_cubit_state.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'todo_overview_cubit.dart';
+
+class TodoOverviewCubitState extends Equatable {
+  const TodoOverviewCubitState();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
